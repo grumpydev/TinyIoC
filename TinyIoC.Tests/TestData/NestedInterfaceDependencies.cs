8a53dece605967adb12f625d62f07a39c49ec657
srobbins <>
2010-03-04 06:42:35 +0000

    Added licence blurb to the source files. Seemed like a sensible thing to do :-)

--- TinyIoC.Tests/TestData/NestedInterfaceDependencies.cs
+++ TinyIoC.Tests/TestData/NestedInterfaceDependencies.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿//===============================================================================
+// TinyIoC
+//
+// An easy to use, hassle free, Inversion of Control Container for small projects
+// and beginners alike.
+//
+// http://hg.grumpydev.com/tinyioc
+//===============================================================================
+// Copyright © Steven Robbins.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+//===============================================================================
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
