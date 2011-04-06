9c0ab0e39c51c0022215633b3ffec139f7bcaff9
Andy Sherwood <xinmyname@gmail.com>
2011-04-06 10:16:58 -0700

    Added logo art, nuspecs for aspnetextensions and tiny messenger

--- build.ps1
+++ build.ps1
@@ -1,6 +1,17 @@
 $buildDir = ".\build"
-$outputDir = $buildDir + "\Content"
+$coreDir = $buildDir + "\Core"
+$aspnetDir = $buildDir + "\AspNet"
+$messengerDir = $buildDir + "\Messenger"
+$coreOutputDir = $coreDir + "\Content"
+$aspnetOutputDir = $aspnetDir + "\Content"
+$messengerOutputDir = $messengerDir + "\Content"
 if (test-path $buildDir) { ri -r -fo $buildDir }
-mkdir $outputDir | out-null
-copy .\src\TinyIoC\TinyIoC.cs $outputDir
-.\Tools\nuget\NuGet.exe pack .\TinyIoC.nuspec -b .\build -o .\build
+mkdir $coreOutputDir | out-null
+mkdir $aspnetOutputDir | out-null
+mkdir $messengerOutputDir | out-null
+copy .\src\TinyIoC\TinyIoC.cs $coreOutputDir
+copy .\src\TinyIoC\TinyIoCAspNetExtensions.cs $aspnetOutputDir
+copy .\src\TinyIoC\TinyMessenger.cs $messengerOutputDir
+.\Tools\nuget\NuGet.exe pack .\TinyIoC.nuspec -b $coreDir -o $coreDir
+.\Tools\nuget\NuGet.exe pack .\TinyIoCAspNetExtensions.nuspec -b $aspnetDir -o $aspnetDir
+.\Tools\nuget\NuGet.exe pack .\TinyMessenger.nuspec -b $messengerDir -o $messengerDir
