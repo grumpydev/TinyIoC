9ef13a7555b8bc1ce69a6e18b860247dd2336df7
Steven Robbins <ste.robbins@gmail.com>
2011-08-26 15:39:30 +0100

    Added test from #4

--- src/TinyIoC.Tests/TestData/NestedInterfaceDependencies.cs
+++ src/TinyIoC.Tests/TestData/NestedInterfaceDependencies.cs
@@ -28,29 +28,61 @@ namespace TinyIoC.Tests.TestData
 
         internal interface IService2
         {
+            IService3 Service3 { get; }
         }
 
         internal interface IService3
         {
         }
 
+        internal interface IService4
+        {
+            IService2 Service2 { get; }
+        }
+
+        internal interface IService5
+        {
+            IService4 Service4 { get; }
+        }
+
         internal class Service1 : IService1
         {
         }
-
+        
         internal class Service2 : IService2
         {
-            IService3 service3;
+            public IService3 Service3 { get; private set; }
 
             public Service2(IService3 service3)
             {
-                this.service3 = service3;
+                this.Service3 = service3;
             }
         }
 
         internal class Service3 : IService3
         {
         }
+
+        internal class Service4 : IService4
+        {
+            public IService2 Service2 { get; private set; }
+
+            public Service4(IService2 service1)
+            {
+                Service2 = service1;
+            }
+        }
+
+        internal class Service5 : IService5
+        {
+            public Service5(IService4 service4)
+            {
+                Service4 = service4;
+            }
+
+            public IService4 Service4 { get; private set; }
+        }
+
 
         internal interface IRoot
         {
--- src/TinyIoC.Tests/TinyIoCFunctionalTests.cs
+++ src/TinyIoC.Tests/TinyIoCFunctionalTests.cs
@@ -25,6 +25,8 @@ using NestedClassDependencies = TinyIoC.Tests.TestData.NestedClassDependencies;
 
 namespace TinyIoC.Tests
 {
+    using NestedInterfaceDependencies;
+
     using TinyIoC.Tests.PlatformTestSuite;
 
     [TestClass]
@@ -182,6 +184,29 @@ namespace TinyIoC.Tests
             platformTests.RunTests(out run, out passed, out failed);
 
             Assert.AreEqual(0, failed);
+        }
+
+        [TestMethod]
+        public void Resolve_InterfacesAcrossInChildContainer_Resolves()
+        {
+            var container = UtilityMethods.GetContainer();
+
+            container.Register<IService2, Service2>().AsMultiInstance();
+
+            container.Register<IService4, Service4>().AsMultiInstance();
+
+            container.Register<IService5, Service5>().AsMultiInstance();
+
+            var child = container.GetChildContainer();
+
+            var nestedService = new Service3();
+            child.Register<IService3>(nestedService);
+
+            var service5 = child.Resolve<IService5>();
+
+            Assert.IsNotNull(service5.Service4);
+
+            Assert.AreSame(nestedService, service5.Service4.Service2.Service3);
         }
     }
 }
