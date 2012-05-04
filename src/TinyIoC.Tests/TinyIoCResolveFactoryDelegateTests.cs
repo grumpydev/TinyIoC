3e04cb5d106b90e18d388a9bf3293ce99db7e629
Andrew Davey <andrew@equin.co.uk>
2012-05-04 19:01:05 +0100

    Add support for resolving factory delegates.

---
+++ src/TinyIoC.Tests/TinyIoCResolveFactoryDelegateTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TinyIoC.Tests.TestData;
+
+namespace TinyIoC.Tests
+{
+    [TestClass]
+    public class TinyIoCResolveFactoryDelegateTests
+    {
+        class Example
+        {
+            public delegate Example Factory(int value);
+
+            public Example(int value, Dependency dependency)
+            {
+                Value = value;
+                Dependency = dependency;
+            }
+
+            public int Value { get; private set; }
+            public Dependency Dependency { get; private set; }
+        }
+
+        class Dependency
+        {
+        }
+
+        [TestMethod]
+        public void Resolve_FactoryDelegate_ReturnsDelegateThatCanConstructInstance()
+        {
+            var container = UtilityMethods.GetContainer();
+            
+            var factory = container.Resolve<Example.Factory>();
+            var createdObject = factory(1);
+
+            Assert.AreEqual(1, createdObject.Value);
+            Assert.IsNotNull(createdObject.Dependency);
+        }
+
+        [TestMethod]
+        public void Resolve_FactoryDelegate_CachesDelegate()
+        {
+            var container = UtilityMethods.GetContainer();
+
+            var factory1 = container.Resolve<Example.Factory>();
+            var factory2 = container.Resolve<Example.Factory>();
+
+            Assert.AreSame(factory1, factory2);
+        }
+    }
+}
