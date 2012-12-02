19529af2005b2eaed84f2b1eb317e0cb07c7b3c6
Steven Robbins <ste.robbins@gmail.com>
2012-12-02 16:17:16 +0000

    Tweaks

--- build.ps1
+++ build.ps1
@@ -5,16 +5,18 @@ $messengerDir = $buildDir + "\Messenger"
 $winrtDir = $buildDir + "\WinRT"
 $coreOutputDir = $coreDir + "\Content"
 $aspnetOutputDir = $aspnetDir + "\Content"
+$winrtOutputDir = $winrtDir + "\Content"
 $messengerOutputDir = $messengerDir + "\Content"
 if (test-path $buildDir) { ri -r -fo $buildDir }
 mkdir $coreOutputDir | out-null
 mkdir $aspnetOutputDir | out-null
+mkdir $winrtOutputDir | out-null
 mkdir $messengerOutputDir | out-null
 copy .\src\TinyIoC\TinyIoC.cs $coreOutputDir
-copy .\src\TinyIoC.MetroStyle\TypeExtender.cs $winrtDir
 copy .\src\TinyIoC\TinyIoCAspNetExtensions.cs $aspnetOutputDir
+copy .\src\TinyIoC.MetroStyle\TypeExtender.cs $winrtOutputDir
 copy .\src\TinyIoC\TinyMessenger.cs $messengerOutputDir
 .\Tools\nuget\NuGet.exe pack .\TinyIoC.nuspec -b $coreDir -o $coreDir
-.\Tools\nuget\NuGet.exe pack .\TinyIoCWinRT.nuspec -b $winrtDir -o $winrtDir
 .\Tools\nuget\NuGet.exe pack .\TinyIoCAspNetExtensions.nuspec -b $aspnetDir -o $aspnetDir
+.\Tools\nuget\NuGet.exe pack .\TinyIoCWinRT.nuspec -b $winrtDir -o $winrtDir
 .\Tools\nuget\NuGet.exe pack .\TinyMessenger.nuspec -b $messengerDir -o $messengerDir
