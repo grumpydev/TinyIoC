77572606e4a99e7b8d57b9d01dad2e847086205f
Daniel Lo Nigro <daniel@dan.cx>
2016-10-02 00:19:22 -0700

    Small fixes for .NET Core, and added example to platformtests directory

---
+++ platformtests/NETCore/src/NETCore/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TinyIoC;
+
+namespace NETCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+	        var container = TinyIoCContainer.Current;
+			container.Register<IFoo, Foo>();
+
+	        container.Resolve<IFoo>().SayHello();
+	        Console.ReadKey();
+        }
+    }
+
+	public interface IFoo
+	{
+		void SayHello();
+	}
+
+	public class Foo : IFoo
+	{
+		public void SayHello()
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
