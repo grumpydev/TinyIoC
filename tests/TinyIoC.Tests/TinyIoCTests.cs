grumpydev/TinyIoC

commit 3891ab9428acd6bd8b4c29f6ead7808a50f423ca

Added TinyIoCConstructorAttribute with tests

src/TinyIoC.Tests/TestData/BasicClasses.cs
@@ -381,5 +381,59 @@ namespace TinyIoC.Tests.TestData
                 return new T();
             }
         }
+
+        internal class TestClassWithConstructorAttrib
+        {
+            [TinyIoCConstructor]
+            public TestClassWithConstructorAttrib()
+            {
+                AttributeConstructorUsed = true;
+            }
+
+            public TestClassWithConstructorAttrib(object someParameter)
+            {
+                AttributeConstructorUsed = false;
+            }
+
+            public bool AttributeConstructorUsed { get; private set; }
+        }
+
+        internal class TestClassWithInternalConstructorAttrib
+        {
+            [TinyIoCConstructor]
+            internal TestClassWithInternalConstructorAttrib()
+            {
+                AttributeConstructorUsed = true;
+            }
+
+            public TestClassWithInternalConstructorAttrib(object someParameter)
+            {
+                AttributeConstructorUsed = false;
+            }
+
+            public bool AttributeConstructorUsed { get; private set; }
+        }
+
+        internal class TestClassWithManyConstructorAttribs
+        {
+            [TinyIoCConstructor]
+            public TestClassWithManyConstructorAttribs()
+            {
+                MostGreedyAttribCtorUsed = false;
+            }
+
+            [TinyIoCConstructor]
+            public TestClassWithManyConstructorAttribs(object someParameter)
+            {
+                MostGreedyAttribCtorUsed = true;
+            }
+
+            public TestClassWithManyConstructorAttribs(object a, object b)
+            {
+                MostGreedyAttribCtorUsed = false;
+            }
+
+            public bool MostGreedyAttribCtorUsed { get; private set; }
+        }
     }
 }

src/TinyIoC.Tests/TinyIoCTests.cs
@@ -1626,6 +1626,45 @@ namespace TinyIoC.Tests
         }
 
         [TestMethod]
+        public void Resolve_ConstructorSpecifiedWithAttribute_UsesCorrectCtor()
+        {
+            var container = UtilityMethods.GetContainer();
+            var result = container.Resolve<TestClassWithConstructorAttrib>();
+
+            Assert.IsTrue(result.AttributeConstructorUsed);
+        }
+
+        [TestMethod]
+        public void Resolve_InternalConstructorSpecifiedWithAttribute_UsesCorrectCtor()
+        {
+            var container = UtilityMethods.GetContainer();
+            var result = container.Resolve<TestClassWithInternalConstructorAttrib>();
+
+            Assert.IsTrue(result.AttributeConstructorUsed);
+        }
+
+        [TestMethod]
+        public void Resolve_ManyConstructorsSpecifiedWithAttribute_UsesCorrectCtor()
+        {
+            var container = UtilityMethods.GetContainer();
+            var result = container.Resolve<TestClassWithManyConstructorAttribs>();
+
+            Assert.IsTrue(result.MostGreedyAttribCtorUsed);
+        }
+
+        [TestMethod]
+        public void Resolve_AttributeConstructorOverriden_UsesCorrectCtor()
+        {
+            var container = UtilityMethods.GetContainer();
+            container.Register<TestClassWithConstructorAttrib>()
+                .UsingConstructor(() => new TestClassWithConstructorAttrib(null)); // The non-attribute constructor
+
+            var result = container.Resolve<TestClassWithConstructorAttrib>();
+
+            Assert.IsFalse(result.AttributeConstructorUsed);
+        }
+
+        [TestMethod]
         public void CanResolve_ConstructorSpecifiedThatRequiresParametersButNonePassed_ReturnsFalse()
         {
             var container = UtilityMethods.GetContainer();

src/TinyIoC/TinyIoC.cs
@@ -3975,7 +3975,17 @@ namespace TinyIoC
             //#if NETFX_CORE
             //			return type.GetTypeInfo().DeclaredConstructors.OrderByDescending(ctor => ctor.GetParameters().Count());
             //#else
-            return type.GetConstructors().OrderByDescending(ctor => ctor.GetParameters().Count());
+            var candidateCtors = type.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .Where(x => !x.IsPrivate) // Includes internal constructors but not private constructors
+                .ToList();
+
+            var attributeCtors = candidateCtors.Where(x => x.GetCustomAttributes(typeof(TinyIoCConstructorAttribute), false).Any())
+                .ToList();
+
+            if (attributeCtors.Any())
+                candidateCtors = attributeCtors;
+
+            return candidateCtors.OrderByDescending(ctor => ctor.GetParameters().Count());
             //#endif
         }
 
@@ -4300,4 +4310,14 @@ namespace TinyIoC
         }
     }
 #endif
+
+    [AttributeUsage(AttributeTargets.Constructor, Inherited = false, AllowMultiple = false)]
+#if TINYIOC_INTERNAL
+    internal
+#else
+    public
+#endif
+    sealed class TinyIoCConstructorAttribute : Attribute
+    {
+    }
 }

commit 6f1aa79ed124da8ebe3f15f86beb331316e017b2

Show regression with protected constructor check
Add tests to exercise construction and checking resolution of classes that have
only protected constructors. A new test that asserts that CanResolve of a class with only a protected constructor will return false will fail.  The regression
was introduced in
https://github.com/grumpydev/TinyIoC/commit/3891ab9428acd6bd8b4c29f6ead7808a50f423ca.
The original use of type.GetConstructors() only returend public
constructors - no protected ones.
Resolution of a protected constructor must throw and checking for resolution must also fail.  On the other hand, resolving a concrete subclass of such a class should succeed if the subclass has a public constructor that has its dependencies met.

tests/TinyIoC.Tests/TestData/BasicClasses.cs
@@ -120,6 +120,20 @@ namespace TinyIoC.Tests.TestData
             }
         }
 
+        internal class TestClassProtectedCtor
+        {
+            protected TestClassProtectedCtor()
+            {
+            }
+        }
+
+        internal class TestSubclassProtectedCtor
+        {
+            public TestSubclassProtectedCtor()
+            {
+            }
+        }
+
         internal class TestClassWithParameters
         {
             public string StringProperty { get; set; }

tests/TinyIoC.Tests/TinyIoCTests.cs
@@ -949,7 +949,7 @@ namespace TinyIoC.Tests
 
         [TestMethod]
         //[ExpectedException(typeof(TinyIoCResolutionException))]
-        public void Resolve_ClassWithNoPublicConstructor_ThrowsCorrectException()
+        public void Resolve_ClassWithOnlyPrivateConstructor_ThrowsCorrectException()
         {
             var container = UtilityMethods.GetContainer();
             container.Register<TestClassPrivateCtor>();
@@ -957,6 +957,42 @@ namespace TinyIoC.Tests
             AssertHelper.ThrowsException<TinyIoCResolutionException>(() => container.Resolve<TestClassPrivateCtor>());
 
             //Assert.IsInstanceOfType(result, typeof(TestClassPrivateCtor));
+        }
+
+        [TestMethod]
+        public void Resolve_ClassWithOnlyProtectedConstructor_ThrowsCorrectException()
+        {
+            var container = UtilityMethods.GetContainer();
+            container.Register<TestClassProtectedCtor>();
+
+            AssertHelper.ThrowsException<TinyIoCResolutionException>(() => container.Resolve<TestClassPrivateCtor>());
+        }
+
+        [TestMethod]
+        public void CanResolve_ClassWithOnlyProtectedConstructor_ReturnsFalse()
+        {
+            var container = UtilityMethods.GetContainer();
+            container.Register<TestClassProtectedCtor>();
+
+            bool canResolve = container.CanResolve<TestClassProtectedCtor>();
+            Assert.IsFalse(canResolve);
+        }
+
+        [TestMethod]
+        public void Resolve_SubclassOfClassWithOnlyProtectedConstructor_Succeeds()
+        {
+            var container = UtilityMethods.GetContainer();
+            container.Register<TestSubclassProtectedCtor>();
+        }
+
+        [TestMethod]
+        public void CanResolve_SubclassOfClassWithOnlyProtectedConstructor_ReturnsFalse()
+        {
+            var container = UtilityMethods.GetContainer();
+            container.Register<TestClassProtectedCtor>();
+
+            bool canResolve = container.CanResolve<TestSubclassProtectedCtor>();
+            Assert.IsTrue(canResolve);
         }
 
         [TestMethod]