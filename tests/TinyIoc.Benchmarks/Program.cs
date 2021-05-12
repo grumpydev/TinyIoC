48d6eb4c1aa8f2d291b9ac998b6bd25d85c32e56
Troy Willmot <6725891+Yortw@users.noreply.github.com>
2021-05-12 22:49:45 +1200

    Setup basic benchmarking project.

---
+++ tests/TinyIoc.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace TinyIoc.Benchmarks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+		}
+	}
+}
---
+++ tests/TinyIoc.Benchmarks/ResolveBenchmarks.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace TinyIoc.Benchmarks
+{
+
+#if NET48 //These work on either platform but no point running them twice
+	[SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net48)]
+	[SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net461)]
+#endif
+#if NETCOREAPP3_1_OR_GREATER // These don't seem to work in a FX app
+	[SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.NetCoreApp31)]
+	[RyuJitX86Job]
+#endif
+#if NET48 //These don't seem to work in a .net core app
+	[RyuJitX64Job]
+	[MonoJob]
+#endif
+	[MemoryDiagnoser]
+	public class ResolveBenchmarks
+	{
+		private TinyIoC.TinyIoCContainer _NewContainer;
+		private TinyIoC.Original.TinyIoCContainer _OriginalContainer;
+
+		[BenchmarkDotNet.Attributes.GlobalSetup]
+		public void InitContainer()
+		{
+			_NewContainer = new TinyIoC.TinyIoCContainer();
+			_NewContainer.Register<ISingleton, Singleton>().AsSingleton();
+			_NewContainer.Register<IParameterlessInstance, ParameterlessInstance>().AsMultiInstance();
+			_NewContainer.Register<ISingleParameterInstance, SingleParameterInstance>().AsMultiInstance();
+
+			_OriginalContainer = new TinyIoC.Original.TinyIoCContainer();
+			_OriginalContainer.Register<ISingleton, Singleton>().AsSingleton();
+			_OriginalContainer.Register<IParameterlessInstance, ParameterlessInstance>().AsMultiInstance();
+			_OriginalContainer.Register<ISingleParameterInstance, SingleParameterInstance>().AsMultiInstance();
+		}
+
+		[Benchmark]
+		public ISingleton Original_Resolve_Singleton()
+		{
+			return _OriginalContainer.Resolve<ISingleton>();
+		}
+
+		[Benchmark]
+		public ISingleton New_Resolve_Singleton()
+		{
+			return _NewContainer.Resolve<ISingleton>();
+		}
+
+
+		[Benchmark]
+		public IParameterlessInstance Original_Resolve_Instance_Without_Dependencies()
+		{
+			return _OriginalContainer.Resolve<IParameterlessInstance>();
+		}
+
+		[Benchmark]
+		public IParameterlessInstance New_Resolve_Instance_Without_Dependencies()
+		{
+			return _NewContainer.Resolve<IParameterlessInstance>();
+		}
+
+
+		[Benchmark]
+		public ISingleParameterInstance Original_Resolve_Instance_With_Singleton_Dependency()
+		{
+			return _OriginalContainer.Resolve<ISingleParameterInstance>();
+		}
+
+		[Benchmark]
+		public ISingleParameterInstance New_Resolve_Instance_With_Singleton_Dependency()
+		{
+			return _NewContainer.Resolve<ISingleParameterInstance>();
+		}
+	}
+
+	public interface ISingleton
+	{
+
+	}
+
+	public class Singleton : ISingleton
+	{
+		public Singleton()
+		{
+
+		}
+	}
+
+	public interface IParameterlessInstance { }
+
+	public class ParameterlessInstance : IParameterlessInstance { }
+
+	public interface ISingleParameterInstance { }
+
+	public class SingleParameterInstance : ISingleParameterInstance
+	{
+		private readonly ISingleton _Singleton;
+		public SingleParameterInstance(ISingleton singleton)
+		{
+			_Singleton = singleton;
+		}
+	}
+}
